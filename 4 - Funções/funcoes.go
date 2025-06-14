a6aa9bb59c77e2d92ee74039c3888385a410312f
epirescod <epirescodes@gmail.com>
2025-06-14 16:06:36 -0300

    subindo as funções do GO testadas em aula

---
+++ 4 - Funções/funcoes.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//parametros são dados que vc coloca na função pra ela funcionar e retorno é o resultado que ela te da
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+
+	resultado := f("Texto da Função 1")
+	fmt.Println(resultado)
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
+
+}
