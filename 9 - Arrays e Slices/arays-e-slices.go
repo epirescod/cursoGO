a6e8531d7743c0888d8806acdf34192cd59c86df
epirescod <epirescodes@gmail.com>
2025-06-16 18:52:18 -0300

    atualização do arquivo 9 - arrays, onde atualizo as funções de arrays internas.

--- 9 - Arrays e Slices/arays-e-slices.go
+++ 9 - Arrays e Slices/arays-e-slices.go
@@ -25,4 +25,22 @@ func main() {
 	slice2 := array2[1:4]
 	fmt.Println(slice2)
 
+	//Array internos, aprendendo sobre utilizar o MAKE
+
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5) // adicionando mais um valor no slice3
+	slice3 = append(slice3, 6) // adicionando mais um valor no slice3 tornando a capacidade dele pra 12
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //length ou tamanho
+	fmt.Println(cap(slice3)) // capacidade do slice3
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
 }
